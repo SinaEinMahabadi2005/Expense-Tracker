57c9c476599f6bded62cdc1344b81b4c1f5b3fce
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-13 16:01:01 +0330

    add app

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+//Node js part 8 , 9
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import { catchError } from "vanta-api";
+import { exportValidationData } from "./Middlewares/ExportValidation.js";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpace } from "./Utils/Swagger.js";
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(limiter);
+// app.use("/upload", express.static(`${__dirname}/Public`));
+app.use(exportValidationData);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpace));
+
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+app.use(catchError);
+export default app;
