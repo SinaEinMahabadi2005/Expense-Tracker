ee506e11378d527f0661f5f242eddf9582bde077
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-13 16:01:42 +0330

    add export validation data

---
+++ backend/Middlewares/ExportValidation.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+export const exportValidationData = (req, res, next) => {
+  try {
+    const token = req.headers["authorization"]?.split(" ")[1];
+    const { _id, role } = jwt.verify(token, process.env.JWT_SECRET);
+    // Create Key
+    req.userId = _id;
+    req.role = role;
+  } catch (error) {
+    req.userId = null;
+    req.role = null;
+  }
+  next();
+};
