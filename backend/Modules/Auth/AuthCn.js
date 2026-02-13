0dbf20142f3b019d7c3d57cef2b9b3789db0f1cb
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-13 17:11:38 +0330

    add login user

--- backend/Modules/Auth/AuthCn.js
+++ backend/Modules/Auth/AuthCn.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { catchAsync, HandleERROR } from "vanta-api";
 import User from "../User/UserMd.js";
+import jwt from "jsonwebtoken";
 // register
 export const register = catchAsync(async (req, res, next) => {
   const { fullName, password, email } = req.body;
@@ -13,7 +14,21 @@ export const register = catchAsync(async (req, res, next) => {
   });
 });
 // login
-export const login=catchAsync(async (req,res,next) => {
-    const {email , password}=req.body
-    
-})
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const isMatch = bcryptjs.compareSync(password, user.password);
+  if (!isMatch) {
+    return next(new HandleERROR("Email or Password is incorrect"));
+  }
+  const token = jwt.sign(
+    { _id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+  );
+  return res.status(200).json({
+    success: true,
+    message: "login successfully",
+    data: user,
+    token,
+  });
+});
