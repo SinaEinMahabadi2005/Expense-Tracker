1557e19e384d399d96d8b0f912fa9fd70d20646c
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-14 02:57:31 +0330

    debug

--- backend/Modules/Auth/Auth.js
+++ backend/Modules/Auth/Auth.js
@@ -1,11 +1,12 @@
 import Router from "express";
 import { login, register } from "./AuthCn.js";
 import { loginValidator, registerValidator } from "./AuthValidator.js";
+import { handleValidationErrors } from "../../Utils/handleValidationError.js";
 
 
 const authRouter = Router();
 
-authRouter.route("/register").post(registerValidator, register);
-authRouter.route("/login").post(loginValidator, login);
+authRouter.route("/register").post(registerValidator,handleValidationErrors, register);
+authRouter.route("/login").post(loginValidator,handleValidationErrors, login);
 
 export default authRouter;
