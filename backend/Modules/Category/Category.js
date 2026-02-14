148bcd1d7503784de67616d67ea4740505fca84d
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-14 04:34:39 +0330

    update category router

--- backend/Modules/Category/Category.js
+++ backend/Modules/Category/Category.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import isLogin from "../../Middlewares/isLogin.js";
 import { create, getAll, getOne, remove, update } from "./CategoryCn.js";
+import { handleValidationErrors } from "../../Utils/handleValidationError.js";
+import { createValidator, getAllValidator, getOneValidator, removeValidator, updateValidator } from "./CategoryValidator.js";
 const categoryRouter=Router()
-categoryRouter.route("/").get(isLogin,getAll).post(isLogin,create)
-categoryRouter.route("/:id").get(isLogin,getOne).patch(isLogin,update).delete(isLogin,remove)
+categoryRouter
+  .route("/")
+  .get(isLogin, getAllValidator, handleValidationErrors, getAll)
+  .post(isLogin, createValidator, handleValidationErrors, create);
+
+categoryRouter
+  .route("/:id")
+  .get(isLogin, getOneValidator, handleValidationErrors, getOne)
+  .patch(isLogin, updateValidator, handleValidationErrors, update)
+  .delete(isLogin, removeValidator, handleValidationErrors, remove);
 export default categoryRouter
