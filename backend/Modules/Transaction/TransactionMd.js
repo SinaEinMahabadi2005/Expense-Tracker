f2d4b28d590534f65ed1f1247adbe15264ca13ea
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-14 05:08:31 +0330

    add transaction model

--- backend/Modules/Transaction/TransactionMd.js
+++ backend/Modules/Transaction/TransactionMd.js
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
 const transactionSchema = new mongoose.Schema(
   {
-    categoryId:{
-        type:mongoose.Schema.Types.ObjectId ,
-        ref:"Category"
-    } ,
- 
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: [true, "title is required"],
+      unique: [true, "title is unique"],
+    },
+    note: {
+      type: String,
+      required: [true, "note is required"],
+    },
+    amount: {
+      type: Number,
+      required: [true, "amount is required"],
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: [true, "type is required "],
+    },
   },
   { timestamps: true },
 );
