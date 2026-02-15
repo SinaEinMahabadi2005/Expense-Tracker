f6ac8c0aa3f4b4ee5b006f29195133932e2384c2
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-16 00:54:52 +0330

    update budget router

--- backend/Modules/Budget/Budget.js
+++ backend/Modules/Budget/Budget.js
@@ -1,7 +1,17 @@
 import Router from "express"
 import isLogin from "../../Middlewares/isLogin.js"
 import { create, getAll, getOne, remove, update } from "./BudgetCn.js"
+import { createValidator, getAllValidator, getOneValidator, removeValidator, updateValidator } from "./BudgetValidator.js";
+import { handleValidationErrors } from "../../Utils/handleValidationError.js";
 const budgetRouter=Router()
-budgetRouter.route("/").get(isLogin,getAll).post(isLogin,create)
-budgetRouter.route("/:id").get(isLogin,getOne).patch(isLogin,update).delete(isLogin,remove)
+budgetRouter
+  .route("/")
+  .get(isLogin, getAllValidator, handleValidationErrors, getAll)
+  .post(isLogin, createValidator, handleValidationErrors, create);
+
+budgetRouter
+  .route("/:id")
+  .get(isLogin, getOneValidator, handleValidationErrors, getOne)
+  .patch(isLogin, updateValidator, handleValidationErrors, update)
+  .delete(isLogin, removeValidator, handleValidationErrors, remove);
 export default budgetRouter
