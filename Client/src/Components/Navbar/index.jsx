6d2a35a5725d1fcc301bdb3107fd5459d79f7fc0
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2026-02-16 01:33:36 +0330

    add initial page

--- Client/src/Components/Navbar/index.jsx
+++ Client/src/Components/Navbar/index.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
-      <h1>سین جیم</h1>
+      <h1>Cin Mony </h1>
       <div>
       <Link to="/">Home</Link>
       <Link to="/auth">Login/Register</Link>
       <Link to="/profile">Profile</Link>
-      <Link to="/exercises/all/all/all-exercise"></Link>
-      <Link to="/workoutlist">برنامه های تمرینی من</Link>
+      <Link to="/categories/all/all">Categories</Link>
+      <Link to="/transactions">Transactions</Link>
       <Link to="/reports">Reports</Link>
       </div>
     </div>
